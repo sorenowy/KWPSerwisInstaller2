3d149509ea44b59fccdf1db62e2c456400b74dd3
Hubert Kuszyński <hkuszynski@gmail.com>
2019-07-22 19:09:46 +0200

    Commit 0.0.0

---
+++ KWPSerwisInstaller/KWPSerwisInstaller/Interfejsy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KWPSerwisInstaller
+{
+    interface IInstalacjaBazy
+    {
+        void InstalacjaBazy();
+    }
+    interface IInstalacjaInternet
+    {
+        void InstalacjaInternet();
+    }
+    interface IInstalacjaPSTD
+    {
+        void InstalacjaPSTD();
+    }
+    interface IIPConfigLog
+    {
+        void GenerujIPConfigLog();
+    }
+}
